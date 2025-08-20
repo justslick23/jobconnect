e11e24217d06a7d9668c03541049ce853f574195
Tokelo Foso <tokelo.foso@cbs.co.ls>
2025-08-20 12:31:53 +0200

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'role_id'];
 
     protected $hidden = ['password', 'remember_token'];
 
